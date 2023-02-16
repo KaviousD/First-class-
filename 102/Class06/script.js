661479a13c8d27b69f3062a514645c68fc1874d3
KD <123972794+KaviousD@users.noreply.github.com>
2023-02-16 00:02:30 -0600

    upload

---
+++ 102/Class06/script.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var txt;
+    if (confirm("You've successfully logged in!")) {
+      txt = "You've successfully logged in";
+    } else {
+      txt = "You pressed Cancel!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+  }
+  function myFunction() {
+    var txt;
+    if (confirm(" Welcome HiddenK@gmail.com you have successfully logged in !")) {
+      txt = "welcome HiddenK@gmail.com";
+    } else {
+      txt = "You pressed Cancel!";
+    }
+    document.getElementById(" welcome HiddenK@gmail.com").innerHTML = txt;
+  }
+  if (confirm("Would you like to continue to a digital nomad's oasis ?")) {
+    document.write("Thanks for viewing :) ");
+  } else {
+    document.write("OK, Goodbye");
+  }
